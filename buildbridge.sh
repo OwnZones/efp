7dfa807bfa90c483b11115702daec1c9be973997
Anders Cedronius <anders.cedronius@icloud.com>
2020-02-27 23:45:51 +0100

    added build bridge script used by swift bridging header

--- buildbridge.sh
+++ buildbridge.sh
@@ -5,3 +5,9 @@
 p == 1 { print }
 /CutPointBridgeON/ { p = 1 }
 ' ElasticFrameProtocol.h > efpbridge.h
+
+    awk '
+/CutPointBridgeON/ { p = 1 }
+p == 0 { print }
+/CutPointBridgeOFF/ { p = 0 }
+' ElasticFrameProtocol.h > efpbridgeinternal.h
